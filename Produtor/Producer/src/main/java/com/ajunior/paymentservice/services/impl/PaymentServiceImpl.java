3bdfa49b61a7a80326df9b7ed90722a729ff4d6f
Amauri Junior <apojr98@gmail.com>
2022-07-26 15:26:46 -0300

    Add producer config

--- Produtor/Producer/src/main/java/com/ajunior/paymentservice/services/impl/PaymentServiceImpl.java
+++ Produtor/Producer/src/main/java/com/ajunior/paymentservice/services/impl/PaymentServiceImpl.java
@@ -1,20 +1,31 @@
 package com.ajunior.paymentservice.services.impl;
 
+import java.io.Serializable;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 
 import com.ajunior.paymentservice.entities.Payment;
 import com.ajunior.paymentservice.services.PaymentService;
+
+import lombok.SneakyThrows;
 
 @Service
 public class PaymentServiceImpl implements PaymentService{
 
 	private static Logger logger = LoggerFactory.getLogger(PaymentServiceImpl.class);
 	
+	@Autowired
+	private KafkaTemplate<String, Serializable> kafkaTemplate;
+	
+	@SneakyThrows
 	@Override
 	public void sendPayment(Payment payment) {
-		logger.info("PAYMENT_SERVICE_IMPL ::: Recebi o pagamento {}", payment);
+		logger.info("Recebi o pagamento {}", payment);
+		logger.info("Enviando pagamento...");
+		kafkaTemplate.send("payment-topic", payment);
 	}
-
 }
